d2699409fa3c1b47f3866eba03a669ea1bc206b8
C0mplexity0 <106925362+C0mplexity0@users.noreply.github.com>
2024-10-28 10:00:08 +0000

    Fix pattern issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+shopt -s extglob
 
 set -e
 set -x
 
-for i in $INPUT_SOURCE_PATTERN; do
+for file in $INPUT_SOURCE_PATTERN; do
     INPUT_SOURCE_FILE=$i
     export INPUT_SOURCE_FILE
     ./copy.sh
